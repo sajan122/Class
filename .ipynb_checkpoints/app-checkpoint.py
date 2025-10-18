530056c713b308fe9d52d598063be15d1d096978
sajan122 <Sajan@123@gmail.cm>
2025-10-18 13:51:15 +0530

    hgshgchsg

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -3,13 +3,10 @@ from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
-import logging 
-import pymongo
-
+import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 app = Flask(__name__)
-
 
 @app.route("/", methods = ['GET'])
 def homepage():
@@ -25,15 +22,15 @@ def index():
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.find_all("div", {"class":"cPHDOP col-12-12"})
-            del bigboxes[0:2]
+            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            del bigboxes[0:3]
             box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.find("a", {"class":"CGtC98"})['href'] 
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
-           
-            commentboxes = prod_html.find_all("div",{"class" :"col EPCmJX"})
+            print(prod_html)
+            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
@@ -43,14 +40,14 @@ def index():
             for commentbox in commentboxes:
                 try:
                     #name.encode(encoding='utf-8')
-                    name = commentbox.find("p",{"class":'_2NsDsF AwS1CA'}).text
+                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
 
                 except:
                     logging.info("name")
 
                 try:
                     #rating.encode(encoding='utf-8')
-                    rating = commentbox.find("div", {"class":"XQDdHH Ga3i8K"}).text
+                    rating = commentbox.div.div.div.div.text
 
 
                 except:
@@ -59,42 +56,31 @@ def index():
 
                 try:
                     #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.find("p", {"class" : "z9E0IG"}).text
+                    commentHead = commentbox.div.div.div.p.text
 
                 except:
                     commentHead = 'No Comment Heading'
                     logging.info(commentHead)
                 try:
-                    custComment = commentbox.find('div', {'class': ''}).text
-                    
+                    comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    #custComment.encode(encoding='utf-8')
+                    custComment = comtag[0].div.text
                 except Exception as e:
-                    custComment = "No Comment"
                     logging.info(e)
-
 
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
-            #For example:@ → %40 # → %23! → %21
-            client = pymongo.MongoClient("mongodb+srv://sajan:Sajan%40123@cluster0.5u8yykc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    
-            db = client['review_scrap']
-            review_col = db['review_scrap_data']
-            review_col.insert_many(reviews)
-            return render_template('result.html', reviews=reviews)
+            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
             return 'something is wrong'
     # return render_template('results.html')
 
     else:
-    
         return render_template('index.html')
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(host="0.0.0.0")
-
-
-
